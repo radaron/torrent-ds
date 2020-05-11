19b0d9281915d0c38a3b16a265dc0cec853fa2c2
Radics Aron Jozsef <radics.aron.jozsef.gmail.com>
2020-05-11 15:51:24 +0200

    read data tool updated

--- tools/read_data.py
+++ tools/read_data.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import json
+import argparse
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
 from torrentds.data import (
         global_init,
@@ -7,21 +9,45 @@ from torrentds.data import (
         Torrent
 )
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--format', '-f',
+                    metavar='FORMAT',
+                    type=str,
+                    choices=['table', 'json'],
+                    required=True,
+                    help="Output format, should be 'json' or 'table'.")
+
+args = parser.parse_args()
 
 global_init(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "database.sqlite"))
 
 session = create_session()
 
-torrents = session.query(Torrent).all()
+torrents = session.query(Torrent).order_by(Torrent.date.desc()).all()
 
-row = "|{:^50}|{:^50}|{:^50}|"
-separate = "{}".format('-'*154)
 
-print(separate)
-print(row.format("Label", "Title", "Date"))
-print(separate)
-for t in torrents:
+if args.format == "table":
+    row = "|{:^50}|{:^60}|{:^50}|{:^15}|{:^15}|"
+    separate = "{}".format('-'*196)
+
     print(separate)
-    print(row.format(t.label, t.title, str(t.date)))
-print(separate)
+    print(row.format("Label", "Title", "Date", "Tracker id", "Client id"))
+    print(separate)
+    for t in torrents:
+        print(separate)
+        print(row.format(t.label, t.title, str(t.date), t.tracker_id,
+                         t.transmission_id))
+    print(separate)
 
+elif args.format == "json":
+    json_data = {}
+    for t in torrents:
+        if t.label not in json_data:
+            json_data[t.label] = []
+        json_data[t.label].append({
+            "title": t.title,
+            "tracker_id": t.tracker_id,
+            "client_id": t.transmission_id,
+            "date": str(t.date)
+        })
+    print(json.dumps(json_data, indent=2))
