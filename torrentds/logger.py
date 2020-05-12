dd5050b144d10c00639b65e784571a4439c601e0
Radics Aron Jozsef <radics.aron.jozsef.gmail.com>
2020-05-12 12:27:42 +0200

    removed white spaces

--- torrentds/logger.py
+++ torrentds/logger.py
@@ -1,12 +1,13 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+
 def init_logger(name, log_path):
     log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
-    my_handler = RotatingFileHandler(log_path, mode='a', maxBytes=5*1024*1024, 
+    my_handler = RotatingFileHandler(log_path, mode='a', maxBytes=5*1024*1024,
                                      backupCount=4, encoding=None, delay=0)
-    
+
     my_handler.setFormatter(log_formatter)
 
     logger = logging.getLogger(name)
