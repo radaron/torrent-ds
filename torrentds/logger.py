dccaff0b35407cdc71216c3e4a0a2346d4875b1e
Radics Aron Jozsef <radics.aron.jozsef.gmail.com>
2020-05-13 17:40:17 +0200

    Logging format was updated

--- torrentds/logger.py
+++ torrentds/logger.py
@@ -3,7 +3,8 @@ from logging.handlers import RotatingFileHandler
 
 
 def init_logger(name, log_path):
-    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    log_formatter = logging.Formatter("%(asctime)s  %(module)s:%(funcName)s "
+                                      "[%(levelname)s] %(message)s")
 
     my_handler = RotatingFileHandler(log_path, mode='a', maxBytes=5*1024*1024,
                                      backupCount=4, encoding=None, delay=0)
