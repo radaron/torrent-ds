19cc38b103d22e83b60bc2f19186370ba4687225
Aron Radics <radics.aron.jozsef@gmail.com>
2022-11-25 21:48:45 +0100

    Fix

--- torrent_ds/logger.py
+++ torrent_ds/logger.py
@@ -1,8 +1,8 @@
 import logging
-
+from logging.handlers import SysLogHandler
 
 def init_logger():
-    syslog = logging.handlers.SysLogHandler(address="/dev/log")
+    syslog = SysLogHandler(address="/dev/log")
     syslog.setFormatter(logging.Formatter('%(name)s: %(levelname)s %(message)s'))
     logger = logging.getLogger("torrent-ds")
     logger.addHandler(syslog)
